ac7cac7d881e8b8c1506c40460f64bf88cbbf8c5
Michael Hardin <hardinm78@gmail.com>
2024-05-29 09:34:42 -0500

    added description, image and links

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import pandas
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
@@ -13,3 +15,21 @@ with col2:
     Hi, I'm a portfolio website developed by <NAME>.   
     """
     st.info(content)
+
+content2 = "Below you can find apps ive made."
+st.write(content2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
+        st.info(row['description'] + " " + f"[Source Code]({row['url']})")
+        st.image(str("images/" + row['image']), use_column_width=True)
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row['title'])
+        st.info(row['description'] + " " + f"[Source Code]({row['url']})")
+        st.image(str("images/" + row['image']), use_column_width=True)
